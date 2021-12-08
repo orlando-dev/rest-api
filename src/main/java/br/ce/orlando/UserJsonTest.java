b3f5f3ad77c4c307304bc184c1a8bc16fba1d635
Orlando <orlando.junior07@hotmail.com>
2021-12-07 21:04:52 -0300

    Json primeiro nivel

---
+++ src/main/java/br/ce/orlando/UserJsonTest.java
@@ -0,0 +1,46 @@
+package br.ce.orlando;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class UserJsonTest {
+	
+	@Test
+	public void deveVerificarPrimeiroNivel() {
+		
+		RestAssured.given()
+		.when()
+			.get("https://restapi.wcaquino.me/users/1")
+		.then()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", containsString("Silva"))
+			.body("age", greaterThan(18));
+	}
+	
+	@Test
+	public void deveVerificarPrimeiroNivelOutrasFormas() {
+		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/users/1");
+		
+		//path
+		Assert.assertEquals(new Integer(1), response.path("id"));
+		Assert.assertEquals(new Integer(1), response.path("%s", "id"));
+		
+		//jsonpath
+		JsonPath jpath = new JsonPath(response.asString());
+		Assert.assertEquals(1, jpath.getInt("id"));
+		
+		//from
+		int id = JsonPath.from(response.asString()).getInt("id");
+		Assert.assertEquals(1, id);
+	}
+}
