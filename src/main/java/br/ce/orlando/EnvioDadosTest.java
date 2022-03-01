e6b1c3b18a3901d3ad287282e31fd5b0fb6a957c
Orlando <orlando.junior07@hotmail.com>
2022-03-01 19:41:31 -0300

    Header

--- src/main/java/br/ce/orlando/EnvioDadosTest.java
+++ src/main/java/br/ce/orlando/EnvioDadosTest.java
@@ -39,4 +39,19 @@ public class EnvioDadosTest {
 			.contentType(Matchers.containsString("utf-8"))
 		;
 	}
+	
+	@Test
+	public void deveEnviarValorViaHeader() {
+		
+		given()
+			.log().all()
+			.accept(ContentType.XML)
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+		;
+	}
 }
