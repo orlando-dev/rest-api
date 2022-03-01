d5df50ac59752bd6d0b65522cbc8d57f8b82fcc6
Orlando <orlando.junior07@hotmail.com>
2022-03-01 19:34:13 -0300

    Query

---
+++ src/main/java/br/ce/orlando/EnvioDadosTest.java
@@ -0,0 +1,24 @@
+package br.ce.orlando;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+
+public class EnvioDadosTest {
+
+	@Test
+	public void deveEnviarValorViaQuery() {
+		
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users?format=json")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.JSON)
+		;
+	}
+}
