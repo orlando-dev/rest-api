fbfa5437d38ac07699c3f9223962e0892f0e410e
Orlando <orlando.junior07@hotmail.com>
2022-04-26 23:01:05 -0300

    Validando esquema - XML

---
+++ src/main/java/br/ce/orlando/SchemaTest.java
@@ -0,0 +1,37 @@
+package br.ce.orlando;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.Test;
+import org.xml.sax.SAXParseException;
+
+import io.restassured.matcher.RestAssuredMatchers;
+
+public class SchemaTest {
+	
+	@Test
+	public void deveValidarSchemaXML() {
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/usersXML")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
+		;
+	}
+	
+	@Test(expected = SAXParseException.class)
+	public void naoDeveValidarSchemaXMLInvalido() {
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/invalidUsersXML")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
+		;
+	}
+}
